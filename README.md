f7155c11893d82e4d5ab65dd1a32089e541fa1d2
Xenvar2307 <lukaszkniemczyk@gmail.com>
2024-01-23 18:53:25 +0100

    test notes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # WP-project
 Project for course Wzorce Projektowe - turn based game build in PyGame
+
+1. Bigger ideas for future
+Empowered versions of abilities related to flow of magic
--- tests.py
+++ tests.py
@@ -1,13 +1,40 @@
 import unittest
 
 
-class TestClass(unittest.TestCase):
+class TestActionPanel(unittest.TestCase):
+    # test if notify gets correct classes
+    # if image is always loaded and correct
     def test_something(self):
         self.assertEqual(1, 1)
 
 
-class TestSomethingDifferent(unittest.TestCase):
+class TestCharacter(unittest.TestCase):
+    # test play animation:
+    # number of frames played, return to idle or staying dead
+
+    # automatic updating healthbar, assigning and removing healthbars
+    # do it together with take damage and heal methods
+    # check if reset is the same as new character with base state
+
     def test_nothing(self):
+        self.assertAlmostEqual(1, 1)
+
+
+class TestDamageTexts(unittest.TestCase):
+    # check if damage text dies after said time
+    def test_something(self):
+        self.assertAlmostEqual(1, 1)
+
+
+class TestHealthBar(unittest.TestCase):
+    # check raw notify
+    def test_something(self):
+        self.assertAlmostEqual(1, 1)
+
+
+class TestHealthBar(unittest.TestCase):
+    # check raw notify
+    def test_something(self):
         self.assertAlmostEqual(1, 1)
 
 
